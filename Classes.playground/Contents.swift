979522707691d91174e0888d6c3bc225c0515ac9
Pradeep <pradeepreddy.kypa@accoliteindia.com>
2020-05-03 07:18:46 +0530

    Adding the Classes Playground File

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+/*
+
+ Classes are similar to structs but support a powerful feature called Inheritance. There are lot of differences between Classes and Structs
+ 
+--> Property Observers
+--> Initialisers
+--> Mutating
+--> Lazy Properties
+--> Access Controls
+--> Static Properties
+
+*/
