7cf3413a8abf8d87b3e2b99766f0dff37cce68b2
Pradeep <pradeepreddy.kypa@accoliteindia.com>
2020-05-03 07:25:02 +0530

    Updating the README File

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -4,11 +4,11 @@ import UIKit
 
  Classes are similar to structs but support a powerful feature called Inheritance. There are lot of differences between Classes and Structs
  
---> Property Observers
---> Initialisers
---> Mutating
---> Lazy Properties
---> Access Controls
---> Static Properties
+-->
+-->
+-->
+-->
+-->
+-->
 
 */
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Classes
  All about Classes in Swift
+
+/*
+
+ Classes are similar to structs but support a powerful feature called Inheritance. There are lot of differences between Classes and Structs
+ 
+-->
+-->
+-->
+-->
+-->
+-->
+
+*/
